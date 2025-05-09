db4654924b1beb670fc29ccb73ced728e003f410
hummusaki <gabriel@gabmort.me>
2025-05-08 22:37:41 -0700

    forgot ==

--- app/requirements.py
+++ app/requirements.py
@@ -8,10 +8,10 @@ def check_oci_cli():
     else:
         print("OCI is not installed")
         choice = input("Install now? [y/n]: ").strip().lower() or "y"
-        if choice = "y":
+        if choice == "y":
             os.system("curl -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.py -o install.py")
             code = os.system(f"{sys.executable} install.py")
-            if code = 0 and shutil.which("oci"):
+            if code == 0 and shutil.which("oci"):
                 print("OCI CLI installed.")
                 return True
         print("OCI CLI Unavailable. Exiting.")
