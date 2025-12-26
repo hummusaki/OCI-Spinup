cf6fa7948ae95c8b60eec8e25f6613a9fc7827c3
Gabriel <gabriel@gabmort.me>
2025-12-26 11:32:38 -0800

    typo

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ def setup():
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    print("Welcome to the Oracle Compute Instance Manager!\n")
+    print("Welcome to the Oracle Compute Instance manager!\n")
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--skip-config", action="store_true",
